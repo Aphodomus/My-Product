9eb6f42d1ca3829fe4697633816841b0bcf73c89
Pablo Silva <pablo.silva@brius.com.br>
2021-12-17 10:05:24 -0300

    Parallax in build

--- src/parallax.js
+++ src/parallax.js
@@ -1,9 +1,9 @@
-document.addEventListener("mousemove", parallax);
+document.addEventListener("wheel", parallax);
 function parallax(e) {
-    document.querySelectorAll(".object").forEach(function(move){
-        var moving_value = move.getAttribute("data-value");
+    document.querySelectorAll(".object").forEach(function(wheel){
+        var moving_value = wheel.getAttribute("data-value");
         var x = (e.clientX * moving_value) /250;
-        var y = (e.clientY * moving_value) /250;
-        move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+        var y = (e.clientY * moving_value) /500;
+        wheel.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
     });
 }
