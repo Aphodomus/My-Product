e6eb18b4183ebe0920866fe694dd2acea75c825d
Daniel Vitor de Oliveira Santos <daniel.santos@brius.com.br>
2021-12-19 18:29:37 -0300

    Add parallax effect using rellax in div welcome and product for you

--- src/parallax.js
+++ src/parallax.js
@@ -1,9 +1,1 @@
-document.addEventListener("wheel", parallax);
-function parallax(e) {
-    document.querySelectorAll(".object").forEach(function(wheel){
-        var moving_value = wheel.getAttribute("data-value");
-        var x = (e.clientX * moving_value) /250;
-        var y = (e.clientY * moving_value) /500;
-        wheel.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
-    });
-}
+var rellax = new Rellax('.rellax');
